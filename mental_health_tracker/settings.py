c357a7046017b6b53ca20ce9f16f6b16db0090c3
luticakep <kaylasorayadj@gmail.com>
2024-08-28 18:55:52 +0700

    fix pws url

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vwsgm26cozgl8_p^i%(o4j4sx+nh^-tnd4&4mgwyj@ioo)k*-_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://kayla-soraya-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kayla-soraya-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition

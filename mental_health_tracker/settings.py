8175b1cff4b86f6ebcd6e30bc1e025b0cab25690
luticakep <kaylasorayadj@gmail.com>
2024-09-11 11:34:31 +0700

    update tutorial2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://kayla-soraya-mentalhealthtracker.pbp.cs.ui.ac.id", "https://kayla-soraya-mentalhealthtracker.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","kayla-soraya-mentalhealthtracker.pbp.cs.ui.ac.id"]
